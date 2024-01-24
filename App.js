503b001761fb4cbc782fb23773174b165f3692c1
LaZy1220 <4elove4iska@gmail.com>
2024-01-24 13:36:12 +0300

    add images and title

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import styled from "styled-components/native";
 
 const Post = styled.View`
-  padding: 15px;
-  border-bottom-width: 3px;
+  padding: 10px;
+  border-bottom-width: 2px;
   border-bottom-color: rgba(0, 0, 0, 0.1);
   border-bottom-style: solid;
 `;
 
+const PostImage = styled.Image`
+  width: 100px;
+  height: 100px;
+`;
+
+const TitleText = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: 700;
+`;
+
 export default function App() {
   return (
-    <View>
-      <Post />
+    <ScrollView style={{ paddingTop: 20 }}>
+      <TitleText>Pokemon's</TitleText>
+      <Post>
+        <PostImage
+          source={{ uri: "https://pokemongolife.ru/p/Bulbasaur.png" }}
+        />
+      </Post>
+      <Post>
+        <PostImage
+          source={{ uri: "https://pokemongolife.ru/p/Charmander.png" }}
+        />
+      </Post>
+      <Post>
+        <PostImage
+          source={{ uri: "https://pokemongolife.ru/p/Caterpie.png" }}
+        />
+      </Post>
+      <Post>
+        <PostImage
+          source={{ uri: "https://pokemongolife.ru/p/Squirtle.png" }}
+        />
+      </Post>
+      <Post>
+        <PostImage source={{ uri: "https://pokemongolife.ru/p/Weedle.png" }} />
+      </Post>
+      <Post>
+        <PostImage source={{ uri: "https://pokemongolife.ru/p/Pidgey.png" }} />
+      </Post>
+      <Post>
+        <PostImage source={{ uri: "https://pokemongolife.ru/p/Rattata.png" }} />
+      </Post>
+      <Post>
+        <PostImage source={{ uri: "https://pokemongolife.ru/p/Fearow.png" }} />
+      </Post>
+      <Post>
+        <PostImage source={{ uri: "https://pokemongolife.ru/p/Ekans.png" }} />
+      </Post>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
